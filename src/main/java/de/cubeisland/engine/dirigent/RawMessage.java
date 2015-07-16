0128ce110bf0f2a29388845e0bd369350fe373f1
Anselm Brehme <development.anselm@breh.me>
2015-07-16 21:04:19 +0200

    rename to dirigent

---
+++ src/main/java/de/cubeisland/engine/dirigent/RawMessage.java
@@ -0,0 +1,73 @@
+/**
+ * The MIT License
+ * Copyright (c) 2013 Cube Island
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package de.cubeisland.engine.dirigent;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class RawMessage implements Iterator<Character>, Iterable<Character>
+{
+    private final char[] message;
+    private int i = -1;
+    private int checkpoint = 0;
+
+    public RawMessage(String message)
+    {
+        this.message = message.toCharArray();
+    }
+
+    public Iterator<Character> iterator()
+    {
+        return this;
+    }
+
+    public boolean hasNext()
+    {
+        return message.length > i + 1;
+    }
+
+    public Character next()
+    {
+        return message[++i];
+    }
+
+    public void prev()
+    {
+        i--;
+    }
+
+    public Character current()
+    {
+        return message[i];
+    }
+
+    public String fromCheckPoint()
+    {
+        return new String(Arrays.copyOfRange(message, checkpoint, message.length));
+    }
+
+    public void setCheckPoint()
+    {
+        checkpoint = i;
+    }
+}
---
+++ src/main/java/de/cubeisland/engine/dirigent/parser/formatter/MessageBuilder.java
@@ -0,0 +1,81 @@
+/**
+ * The MIT License
+ * Copyright (c) 2013 Cube Island
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package de.cubeisland.engine.dirigent.parser.formatter;
+
+import de.cubeisland.engine.dirigent.parser.component.ChainedComponent;
+import de.cubeisland.engine.dirigent.parser.component.ErrorComponent;
+import de.cubeisland.engine.dirigent.parser.component.FoundFormatter;
+import de.cubeisland.engine.dirigent.parser.component.MessageComponent;
+import de.cubeisland.engine.dirigent.parser.component.Text;
+
+public abstract class MessageBuilder<MessageT, BuilderT>
+{
+    public abstract BuilderT newBuilder();
+    public abstract MessageT finalize(BuilderT builderT);
+
+    public abstract void build(Text component, BuilderT builder);
+
+    public void buildAny(MessageComponent component, BuilderT builder)
+    {
+        if (component instanceof FoundFormatter)
+        {
+            buildFormatted(((FoundFormatter)component), builder);
+        }
+        else if (component instanceof ChainedComponent)
+        {
+            buildChain(((ChainedComponent)component), builder);
+        }
+        else if (component instanceof Text)
+        {
+            build(((Text)component), builder);
+        }
+        else if (component instanceof ErrorComponent)
+        {
+            build(((ErrorComponent)component), builder);
+        }
+        else
+        {
+            buildOther(component, builder);
+        }
+        // TODO
+    }
+
+    public final void buildFormatted(FoundFormatter component, BuilderT builder)
+    {
+        @SuppressWarnings("unchecked")
+        MessageComponent processed = component.getFound().process(component.getArg(), component.getContext());
+        buildAny(processed, builder);
+    }
+
+    public abstract void build(ErrorComponent component, BuilderT builder);
+
+    public void buildChain(ChainedComponent chained, BuilderT builder)
+    {
+        for (MessageComponent component : chained.getChained())
+        {
+            buildAny(component, builder);
+        }
+    }
+
+    public abstract void buildOther(MessageComponent component, BuilderT builder);
+}
