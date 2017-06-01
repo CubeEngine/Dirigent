456927cb79b08a67b3b839e5ca6e94a6c783f371
Phillip Schichtel <phillip.public@schich.tel>
2017-06-01 17:20:29 +0200

    load all classes before running tests

--- src/test/java/org/cubeengine/dirigent/perf/SimpleColdToHot.java
+++ src/test/java/org/cubeengine/dirigent/perf/SimpleColdToHot.java
@@ -1,12 +1,36 @@
 package org.cubeengine.dirigent.perf;
 
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.cubeengine.dirigent.formatter.argument.Argument;
+import org.cubeengine.dirigent.formatter.argument.Parameter;
+import org.cubeengine.dirigent.formatter.argument.Value;
 import org.cubeengine.dirigent.parser.Parser;
+import org.cubeengine.dirigent.parser.Text;
+import org.cubeengine.dirigent.parser.component.Component;
+import org.cubeengine.dirigent.parser.component.TextComponent;
+import org.cubeengine.dirigent.parser.element.CompleteMacro;
+import org.cubeengine.dirigent.parser.element.DefaultMacro;
+import org.cubeengine.dirigent.parser.element.Element;
+import org.cubeengine.dirigent.parser.element.Indexed;
+import org.cubeengine.dirigent.parser.element.IndexedDefaultMacro;
+import org.cubeengine.dirigent.parser.element.Macro;
+import org.cubeengine.dirigent.parser.element.NamedMacro;
+
+import static java.util.Arrays.asList;
 
 public class SimpleColdToHot
 {
 
     public static void main(String[] args)
     {
+        List<Class<?>> classes = asList(Parser.class, Macro.class, Element.class, DefaultMacro.class, NamedMacro.class,
+                                        IndexedDefaultMacro.class, Indexed.class, CompleteMacro.class, Argument.class,
+                                        Value.class, Parameter.class, Pattern.class, Matcher.class, Text.class,
+                                        TextComponent.class, Component.class);
+        System.out.println("Classes:");
+        System.out.println(classes);
         String msg = "text and a macro {1:name#with index and comment:and parameter=with value:multiple:and one=more} more text";
         long start, delta;
         long values = 0;
