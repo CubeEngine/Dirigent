44303cd022d872ea8dade3114e9f778e5e10aad9
Phillip Schichtel <phillip.public@schich.tel>
2017-06-01 17:00:36 +0200

    load all classes before running tests

--- src/test/java/org/cubeengine/dirigent/perf/SimpleColdToHot.java
+++ src/test/java/org/cubeengine/dirigent/perf/SimpleColdToHot.java
@@ -1,12 +1,32 @@
 package org.cubeengine.dirigent.perf;
 
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.cubeengine.dirigent.formatter.argument.Argument;
+import org.cubeengine.dirigent.formatter.argument.Parameter;
+import org.cubeengine.dirigent.formatter.argument.Value;
 import org.cubeengine.dirigent.parser.Tokenizer;
+import org.cubeengine.dirigent.parser.token.CompleteMacro;
+import org.cubeengine.dirigent.parser.token.DefaultMacro;
+import org.cubeengine.dirigent.parser.token.Indexed;
+import org.cubeengine.dirigent.parser.token.IndexedDefaultMacro;
+import org.cubeengine.dirigent.parser.token.NamedMacro;
+import org.cubeengine.dirigent.parser.token.Token;
+
+import static java.util.Arrays.asList;
 
 public class SimpleColdToHot
 {
 
     public static void main(String[] args)
     {
+        List<Class<?>> classes = asList(Tokenizer.class, Token.class, DefaultMacro.class, NamedMacro.class,
+                                        IndexedDefaultMacro.class, Indexed.class, CompleteMacro.class, Argument.class,
+                                        Value.class, Parameter.class, Pattern.class, Matcher.class);
+        System.out.println("Classes:");
+        System.out.println(classes);
+
         String msg = "text and a macro {1:name#with index and comment:and parameter=with value:multiple:and one=more} more text";
         long start, delta;
         long values = 0;
